ddf8a698723db335db5f597c48b2c39ba4af19d7
Willhs <whardwicksmith@gmail.com>
2015-04-04 01:51:28 +1300

    basic stuff

---
+++ vic-viewers.js
@@ -0,0 +1,8 @@
+var screenWidth = $(window).width() * 0.8,
+    screenHeight = $(window).height();
+
+var svg = d3.select("body").append("svg")
+            .attr("width", screenWith)
+            .attr('height', screenHeight);
+
+
