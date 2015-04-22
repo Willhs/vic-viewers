a9768831730709392a0444d1659c4f5bfcd4a82a
Willhs <whardwicksmith@gmail.com>
2015-04-22 19:38:32 +1200

    place box changed and extended a little

--- main.js
+++ main.js
@@ -32,41 +32,82 @@ var places = glasses.append("g")
 // Reads in all places
 d3.json("json/places.json", function(error, data){
 
-    var width = 100,
-        height = 100,
-        titleFontSize = 20,
-        descrFontSize =  14;
+    var width = 200,
+        height = 200;
+
+    var paddingHor = 6,
+        paddingVert = 6,
+        innerLeft = paddingHor,
+        innerTop = paddingLeft,
+        innerWidth = width - (paddingHor*2),
+        innerHeight = height - (paddingVert*2);
+
+    var titleFontSize = 20,
+        descrFontSize = 14,
+        titleBaseLine = innerTop + titleFontSize,
+        descrBaseLine = titleBaseLine + titleFontSize,
+        openingHoursBaseLine = descrBaseLine + (3 * descrFontSize);
 
     var place = places.selectAll(".place")
         .data(data)
         .enter()
             .append("g")
-            .attr("class", "place");
+            .attr("class", "place")
+            .attr("transform", function(d){
+                return "translate(" + (d.location.x - (width/2)) + ", " + (d.location.y - (height/2)) + ")";
+            });
 
     place.append("rect")
         .attr("id", "place-box")
         .attr("width", width)
         .attr("height", height)
-        .attr("x", function(d){
-            return d.location.x - (width/2);
-        })
-        .attr("y", function(d){
-            return d.location.y - (height/2);
-        });
+        .attr("x", 0)
+        .attr("y", 0);
+
     // title of the place
     place.append("text")
         .attr("id", "title")
+        .attr("class", "box-text")
         .text(function(d){ return d.title; })
-        .attr("x", function(d){
-            return d.location.x;
-        })
-        .attr("y", function(d){
-            return d.location.y - (height/2) + titleFontSize;
-        })
+        .attr("x", innerLeft)
+        .attr("y", titleBaseLine)
         .style("font-size", titleFontSize + "px")
-        .style("text-anchor", "middle");
+        .each(function(){
+            var bbox = this.getBBox();
+            bbox.width = innerWidth;
+            bbox.height = innerHeight;
+        });
 
-    //place.append()
+    // small description
+    place.append("text")
+        .attr("id", "small-descr")
+        .attr("class", "box-text")
+        .text(function (d){
+            return d.smallDescr;
+        })
+        .attr("x", innerLeft)
+        .attr("y", descrBaseLine)
+        .attr("font-size", descrFontSize)
+        .each(function(){
+            var bbox = this.getBBox();
+            bbox.width = innerWidth;
+            bbox.height = innerHeight / 3; // can take up 1/3 of the space
+        });
+
+    // open hours text
+    place.append("text")
+        .attr("id", "open-until")
+        .attr("class", "box-text")
+        .attr("x", innerLeft)
+        .attr("y", openingHoursBaseLine)
+        .text(function(d){
+            return "Open until: " + d.openingHours;
+        })
+        .each(function(){
+            var bbox = this.getBBox();
+            bbox.width = innerWidth;
+            bbox.height = innerHeight;
+        });
 
 });
 
