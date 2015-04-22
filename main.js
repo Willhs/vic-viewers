cdf57451b3a8b2efefef4cd8a09fa1cd9fae9a65
Willhs <whardwicksmith@gmail.com>
2015-04-23 00:44:48 +1200

    made simple place box and started on selection graphics/animations

--- main.js
+++ main.js
@@ -29,85 +29,125 @@ glasses.append("text")
 var places = glasses.append("g")
     .attr("id", "places");
 
-// Reads in all places
-d3.json("json/places.json", function(error, data){
+function readPlaces(jsonPath){
+    // Reads in all places
+    d3.json(jsonPath, function(error, data){
 
-    var width = 200,
-        height = 200;
+        var width = 200,
+            height = 80;
 
-    var paddingHor = 6,
-        paddingVert = 6,
-        innerLeft = paddingHor,
-        innerTop = paddingLeft,
-        innerWidth = width - (paddingHor*2),
-        innerHeight = height - (paddingVert*2);
+        var paddingHor = 6,
+            paddingVert = 6,
+            innerLeft = paddingHor,
+            innerTop = paddingVert,
+            innerWidth = width - (paddingHor*2),
+            innerHeight = height - (paddingVert*2);
 
-    var titleFontSize = 20,
-        descrFontSize = 14,
-        titleBaseLine = innerTop + titleFontSize,
-        descrBaseLine = titleBaseLine + titleFontSize,
-        openingHoursBaseLine = descrBaseLine + (3 * descrFontSize);
+        var titleRatio = 0.7;
+            openHoursRatio = 0.3;
+            titleBoxHeight = height * titleRatio,
+            openHoursBoxHeight = height * openHoursRatio;
 
-    var place = places.selectAll(".place")
-        .data(data)
-        .enter()
-            .append("g")
-            .attr("class", "place")
-            .attr("transform", function(d){
-                return "translate(" + (d.location.x - (width/2)) + ", " + (d.location.y - (height/2)) + ")";
+        var place = places.selectAll(".place")
+            .data(data)
+            .enter()
+                .append("g")
+                .attr("class", "place")
+                .attr("id", function(d, i){ return "place-" + i;})
+                .attr("transform", function(d){
+                    return "translate(" + (d.location.x - (width/2)) + ", " + (d.location.y - (height/2)) + ")";
+                });
+
+        // box around all place info
+        place.append("rect")
+            .attr("id", "place-box")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 0)
+            .attr("y", 0)
+            // rounded corners
+            .attr("rx", 15)
+            .attr("ry", 15)
+            .attr("stroke-width", 10);
+
+        place.append("rect")
+            .attr("id", "title-box")
+            .attr("width", width)
+            .attr("height", titleBoxHeight)
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("rx", 15)
+            .attr("ry", 15);
+
+        // place icon
+        var placeIconSize = titleBoxHeight - (paddingVert*2);
+        place.append("image")
+            .attr("xlink:href", function(d){
+                // type of place (restaurant, shop ...)
+                var type = d.type;
+                return "res/icon/" + type + ".png";
+            })
+            .attr("x", innerLeft)
+            .attr("y", innerTop)
+            .attr("width", placeIconSize)
+            .attr("height", placeIconSize)
+
+        // title of the place
+        place.append("text")
+            .attr("id", "title")
+            .attr("class", "box-text")
+            .text(function(d){ return d.title; })
+            .attr("x", (paddingHor*2) + placeIconSize)
+            .attr("y", titleBoxHeight - (titleBoxHeight/2) + paddingVert);
+
+        // open hours text
+        place.append("text")
+            .attr("id", "open-until")
+            .attr("class", "box-text")
+            .attr("x", innerLeft)
+            .attr("y", titleBoxHeight + openHoursBoxHeight - paddingVert)
+            .text(function(d){
+                return "Open until: " + d.openingHours;
+            })
+            // keep text within box
+            .each(function(){
+                var bbox = this.getBBox();
+                bbox.width = innerWidth;
+                bbox.height = innerHeight;
             });
+    });
+}
 
-    place.append("rect")
-        .attr("id", "place-box")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("x", 0)
-        .attr("y", 0);
+readPlaces("json/places.json");
 
-    // title of the place
-    place.append("text")
-        .attr("id", "title")
-        .attr("class", "box-text")
-        .text(function(d){ return d.title; })
-        .attr("x", innerLeft)
-        .attr("y", titleBaseLine)
-        .style("font-size", titleFontSize + "px")
-        .each(function(){
-            var bbox = this.getBBox();
-            bbox.width = innerWidth;
-            bbox.height = innerHeight;
-        });
+// makes the place selected which means
+// place is a d3 selection of a node
+function selectPlace(place){
+    var boxIncrease = 20;
+    var placeBoxWidth = place.width + boxIncrease,
+        placeBoxHeight = place.height + boxIncrease;
 
-    // small description
-    place.append("text")
-        .attr("id", "small-descr")
-        .attr("class", "box-text")
-        .text(function (d){
-            return d.smallDescr;
-        })
-        .attr("x", innerLeft)
-        .attr("y", descrBaseLine)
-        .attr("font-size", descrFontSize)
-        .each(function(){
-            var bbox = this.getBBox();
-            bbox.width = innerWidth;
-            bbox.height = innerHeight / 3; // can take up 1/3 of the space
-        });
+    //$(".selected-place").removeClass(".selected-place");
+    place.removeClass(".selected-place");
 
-    // open hours text
-    place.append("text")
-        .attr("id", "open-until")
-        .attr("class", "box-text")
-        .attr("x", innerLeft)
-        .attr("y", openingHoursBaseLine)
-        .text(function(d){
-            return "Open until: " + d.openingHours;
-        })
-        .each(function(){
-            var bbox = this.getBBox();
-            bbox.width = innerWidth;
-            bbox.height = innerHeight;
-        });
+    places.select(place)
+        .attr(".selected-place")
+            .append("rect")
+        .attr("id", "select-outline")
+        .attr("x", -(boxIncrease/2))
+        .attr("y", -(boxIncrease/2))
+        .attr("width", placeBoxWidth + boxIncrease)
+        .attr("height", placeBoxHeight + boxIncrease)
+        .attr("rx", 15)
+        .attr("ry", 15);
 
-});
+    placeBox.transition()
+        .duration(500)
+        .style("opacity", 0.9);
+}
 
+setTimeout(function(){
+    var p0 = $("#place-0");
+    console.log(p0);
+    selectPlace(p0);
+}, 1000);
